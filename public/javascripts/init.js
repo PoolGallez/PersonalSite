e023b3238be0f345ee6a3bd3278b78600c3b5318
Paolo Galletta <galletta.paolo98@gmail.com>
2017-07-05 15:35:45 +0200

    Caricato template principale

---
+++ public/javascripts/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    alert("Prova");
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
